2fa13c5f5121d710349f1b71ab13eaf371b029f9
Pablo Rodriguez Ramos <ramos@ujf.cas.cz>
2021-02-20 20:53:44 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ﻿# Yahoo_stock_price
+Script to get live stock price of different companies obtained from Yahoo finance.
 
--- Yahoo_finance_values.py
+++ Yahoo_finance_values.py
@@ -3,22 +3,19 @@ import urllib
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-stock_name = input ("Enter the stock name: ")
+stock_name = input ("Enter the stock symbol: ")
 
 url = f"https://finance.yahoo.com/quote/{stock_name}?p={stock_name}&.tsrc=fin-srch"
 
 html = urlopen(url)
 
-#html = urlopen("https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch")
-
 contents = html.read()
-#create Soup object
+#Create Soup object
 soup = BeautifulSoup(contents, 'html.parser')
 #define first part of the html structure (main header)
 header_yahoo = soup.find(id='quote-header-info')
-#find in the define class the value of the stock
+#find in the previous object "header_yahoo" the where the price is located
 stock_value = header_yahoo.find('div', class_='D(ib) Va(m) Maw(65%) Ov(h)')
 #print the value
-#print(stock_value.text.strip())
 print(f'Stock value of: {stock_name} is : {stock_value.text.strip()} hitpoints')
 
